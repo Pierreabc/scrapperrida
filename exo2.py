4a94d1247372adc4963ff65efd5837930b797cdf
Pierreabc <pierre.baraban34@gmail.com>
2024-04-17 14:53:35 +0200

    exo2 v2

--- exo2.py
+++ exo2.py
@@ -1,16 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
-
 URL = 'https://www.scrapethissite.com/pages/simple/'
 s = requests.Session()
 r = s.get(URL)
 
-
-
 soup = BeautifulSoup(r.text, 'html.parser')
 
+
+country_tags = soup.find_all('h3', class_='country-name')
 capital_tags = soup.find_all('span', class_='country-capital')
 
-for tag in capital_tags:
-    print(tag.text)
+
+for country, capital in zip(country_tags, capital_tags):
+    print(country.text, '-', capital.text)
